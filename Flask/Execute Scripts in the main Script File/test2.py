a53ee0067fee28876c5c6fc4872911a7016bc2c2
Andrew Dass <dassandrew3@gmail.com>
2022-10-30 15:10:14 -0400

    Rename Flask/Execute Scripts in the main Script File/test2.py to Python/Flask/Execute Scripts in the main Script File/test2.py

--- Flask/Execute Scripts in the main Script File/test2.py
+++
@@ -1,6 +0,0 @@
-def run_a_function():
-    return "Just test 2 running"
-
-print(run_a_function())
-
-# Linking scripts to the main script file is good for organizational purposes and defining functions is preferred in non-main files that runs the flask application, since some functions can contain long code.
---
+++ Python/Flask/Execute Scripts in the main Script File/test2.py
@@ -0,0 +1,6 @@
+def run_a_function():
+    return "Just test 2 running"
+
+print(run_a_function())
+
+# Linking scripts to the main script file is good for organizational purposes and defining functions is preferred in non-main files that runs the flask application, since some functions can contain long code.
