ffedb2c8f37eaac0eb815fec5d052d88ce4886ca
Andrew Dass <dassandrew3@gmail.com>
2022-10-30 15:04:36 -0400

    Update test1.py

--- Flask/Execute Scripts in the main Script File/test1.py
+++ Flask/Execute Scripts in the main Script File/test1.py
@@ -1,8 +1,8 @@
 from flask import Flask
 
 import test2 #as ...
-# Can write test2 as so below can write the new name .run_a_function
-# run_a_function that was defined in test2 and now being called
+# Can write test2 as ... where ... is a new name, so down below can write the new name "".run_a_function
+# run_a_function was the name of the function that was defined in test2 and now it is being called in the format: filename.functionname
 
 app = Flask(__name__)
 
